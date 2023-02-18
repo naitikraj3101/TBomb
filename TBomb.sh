101aa8d20faff578c0ed9a79898a14583b3426fe
naitikraj3101 <76024704+naitikraj3101@users.noreply.github.com>
2023-02-18 14:47:15 +0530

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -32,7 +32,7 @@ banner() {
         figlet TBomb
     fi
     if ! [ -x "$(command -v toilet)" ]; then
-        echo -e "\e[4;34m This Bomber Was Created By \e[1;32m@nora_dav \e[0m"
+        echo -e "\e[4;34m This Bomber Was Created By \e[1;32mNORA \e[0m"
     else
         echo -e "\e[1;34mCreated By \e[1;34m"
         toilet -f mono12 -F border NORA
